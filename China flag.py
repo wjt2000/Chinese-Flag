da0d6b4117f5095a24b03dbe5c8e7bae48f89721
wjt2000 <52854829+wjt2000@users.noreply.github.com>
2022-11-17 20:20:59 +1300

    Add files via upload

---
+++ China flag.py
@@ -0,0 +1,79 @@
+import turtle
+turtle.speed(0)
+myscreen= turtle.Screen()
+
+myscreen.bgcolor('red')
+myscreen.setup(1.0,1.0)
+myscreen.title('Peoples Republic Of China Flag')
+turtle.goto(-600,200)
+
+turtle.pencolor("red")
+turtle.dot(60000)
+
+import turtle as tur
+tur.hideturtle()
+
+tur.pen(pencolor="yellow", pensize=3)
+
+tur.begin_fill()
+
+for i in range(5):
+    tur.forward(200)
+    tur.right(144)
+    tur.fillcolor("yellow")
+
+tur.end_fill()
+
+turtle.penup()
+turtle.goto(-280, 200)
+turtle.pendown()
+tur.pen(pencolor="yellow", pensize=3)
+
+tur.begin_fill()
+
+for i in range(5):
+    tur.forward(50)
+    tur.right(144)
+    tur.fillcolor("yellow")
+
+tur.end_fill()
+turtle.penup()
+turtle.goto(-280, 100)
+turtle.pendown()
+tur.pen(pencolor="yellow", pensize=3)
+
+tur.begin_fill()
+
+for i in range(5):
+    tur.forward(50)
+    tur.right(144)
+    tur.fillcolor("yellow")
+
+tur.end_fill()
+turtle.penup()
+turtle.goto(-350, 0)
+turtle.pendown()
+tur.pen(pencolor="yellow", pensize=3)
+
+tur.begin_fill()
+
+for i in range(5):
+    tur.forward(50)
+    tur.right(144)
+    tur.fillcolor("yellow")
+
+tur.end_fill()
+turtle.penup()
+turtle.goto(-350, 300)
+turtle.pendown()
+tur.pen(pencolor="yellow", pensize=3)
+
+tur.begin_fill()
+
+for i in range(5):
+    tur.forward(50)
+    tur.right(144)
+    tur.fillcolor("yellow")
+
+tur.end_fill()
+turtle.penup()
